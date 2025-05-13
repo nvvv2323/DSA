73f4235b0541336bc90ebf82148c44af4d8d4ab5
Neha vishwakarma <vishwakarmaneha272@gmail.com>
2025-05-13 17:48:53 +0530

    Time: 8 ms (26.82%) | Memory: 43.3 MB (14.06%) - LeetSync

---
+++ 2235-capitalize-the-title/capitalize-the-title.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.split(" ");
+        List<String> result = new ArrayList<>();
+
+        for (String word : words) {
+            if (word.length() <= 2) {
+                result.add(word.toLowerCase());
+            } else {
+                result.add(Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase());
+            }
+        }
+
+        return String.join(" ", result);//list creates of type string that why we join the string lsits by join methos 
+    }
+}
