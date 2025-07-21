8fb44b88123ad3c8f41548bd73a5a1424f23f01c
Neha vishwakarma <vishwakarmaneha272@gmail.com>
2025-07-21 12:12:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (41.95%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -0,0 +1,21 @@
+class Solution {
+    public void merge(int[] arr1, int m, int[] arr2, int n) {
+        
+        int i = m - 1 ;
+        int j = n - 1 ;
+        int k = arr1.length - 1 ;
+
+        while(j >= 0 ){
+
+            if(i >= 0 && arr1[i] > arr2[j]){
+                arr1[k] = arr1[i];
+                k--;
+                i--;
+            }else{
+                arr1[k] = arr2[j];
+                k--;
+                j--;
+            }
+        }
+    }
+}
